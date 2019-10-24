0099c07ae57e69ca6250993986d89af7f3b1a659
Kounah <kounalyn@gmail.com>
2019-10-24 10:19:17 +0200

    working on the base theme configs now

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const Component = require('./src/lib/Component');
 const Builder = require('./src/lib/Builder');
 
+Error.stackTraceLimit = Infinity;
+
 const index = new Component('src/sass', __dirname, {
   isIndex: true
 });
 
 const builder = new Builder();
+
 
 index.build(builder).then(result => console.log(result)).catch(err => console.error(err));
